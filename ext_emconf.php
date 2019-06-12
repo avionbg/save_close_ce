21f5d732d871f1da94ecb6a62bf3f41388f90bdd
Goran Medakovic <goran.it@gmail.com>
2019-06-12 16:44:31 +0200

    - Updated ext version

--- ext_emconf.php
+++ ext_emconf.php
@@ -14,7 +14,7 @@ $EM_CONF[$_EXTKEY] = array (
   'uploadfolder' => false,
   'createDirs' => '',
   'clearCacheOnLoad' => 0,
-  'version' => '1.0.1',
+  'version' => '1.0.3',
   'constraints' => 
   array (
     'depends' => 
