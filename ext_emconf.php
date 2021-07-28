c72c2ab61076241172a3c0c44aee3817b2ff0200
Sven Wappler <info@wappler.systems>
2021-07-28 13:08:03 +0200

    reformatted code updated dependencies

--- ext_emconf.php
+++ ext_emconf.php
@@ -4,30 +4,28 @@
  * Extension Manager/Repository config file for ext "save_close_ce".
  ***************************************************************/
 
-$EM_CONF[$_EXTKEY] = array (
-  'title' => 'Save and close button',
-  'description' => 'Adds save and close button to all the content elements',
-  'category' => 'be',
-  'author' => 'Goran Medakovic',
-  'author_email' => 'avion.bg@gmail.com',
-  'state' => 'stable',
-  'uploadfolder' => false,
-  'createDirs' => '',
-  'clearCacheOnLoad' => 0,
-  'version' => '1.1.0',
-  'constraints' => 
-  array (
-    'depends' => 
-    array (
-      'typo3' => '9.5.0-10.5.0',
-    ),
-    'conflicts' => 
-    array (
-    ),
-    'suggests' => 
-    array (
-    ),
-  ),
-  'clearcacheonload' => false,
-  'author_company' => NULL,
-);
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Save and close button',
+    'description' => 'Adds save and close button to all the content elements',
+    'category' => 'be',
+    'author' => 'Goran Medakovic',
+    'author_email' => 'avion.bg@gmail.com',
+    'state' => 'stable',
+    'uploadfolder' => false,
+    'createDirs' => '',
+    'clearCacheOnLoad' => 0,
+    'version' => '1.2.0',
+    'constraints' =>
+        [
+            'depends' =>
+                [
+                    'typo3' => '9.5.0-11.5.99',
+                ],
+            'conflicts' =>
+                [],
+            'suggests' =>
+                [],
+        ],
+    'clearcacheonload' => false,
+    'author_company' => NULL,
+];
