6a66b3c663cbd14232362b9abc174f98d04a45a3
Sven Wappler <info@wappler.systems>
2021-07-28 13:06:33 +0200

    reformatted code

--- Classes/Hooks/SaveCloseHook.php
+++ Classes/Hooks/SaveCloseHook.php
@@ -11,7 +11,7 @@ use TYPO3\CMS\Backend\Template\Components\Buttons\InputButton;
  * Class SaveButtonHook
  * @package Goran\SaveCloseCe\Hooks
  */
-Class SaveCloseHook
+class SaveCloseHook
 {
     /**
      * @param array $params
@@ -23,27 +23,27 @@ Class SaveCloseHook
         $buttons = $params['buttons'];
         $saveButton = $buttons[\TYPO3\CMS\Backend\Template\Components\ButtonBar::BUTTON_POSITION_LEFT][2][0];
         if ($saveButton instanceof InputButton) {
-          $iconFactory = GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconFactory::class);
-        
-          $saveCloseButton = $buttonBar->makeInputButton()
-            ->setName('_saveandclosedok')
-            ->setValue('1')
-            ->setForm($saveButton->getForm())
-            ->setTitle($this->getLanguageService()->sL('LLL:EXT:core/Resources/Private/Language/locallang_core.xlf:rm.saveCloseDoc'))
-            ->setIcon($iconFactory->getIcon('actions-document-save-close', \TYPO3\CMS\Core\Imaging\Icon::SIZE_SMALL))
-            ->setShowLabelText(true);
+            $iconFactory = GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconFactory::class);
 
-          $buttons[\TYPO3\CMS\Backend\Template\Components\ButtonBar::BUTTON_POSITION_LEFT][2][] = $saveCloseButton;
+            $saveCloseButton = $buttonBar->makeInputButton()
+                ->setName('_saveandclosedok')
+                ->setValue('1')
+                ->setForm($saveButton->getForm())
+                ->setTitle($this->getLanguageService()->sL('LLL:EXT:core/Resources/Private/Language/locallang_core.xlf:rm.saveCloseDoc'))
+                ->setIcon($iconFactory->getIcon('actions-document-save-close', \TYPO3\CMS\Core\Imaging\Icon::SIZE_SMALL))
+                ->setShowLabelText(true);
+
+            $buttons[\TYPO3\CMS\Backend\Template\Components\ButtonBar::BUTTON_POSITION_LEFT][2][] = $saveCloseButton;
         }
         return $buttons;
     }
-    
+
     /**
-	 * Returns the language service
-	 * @return LanguageService
-	 */
-	protected function getLanguageService()
-	{
-		return $GLOBALS['LANG'];
-	}
+     * Returns the language service
+     * @return LanguageService
+     */
+    protected function getLanguageService()
+    {
+        return $GLOBALS['LANG'];
+    }
 }
