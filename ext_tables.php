3f3eef2fad488eeaf00d728aed422c86231dc143
Sven Wappler <info@wappler.systems>
2021-07-28 13:06:10 +0200

    reformatted code

--- ext_tables.php
+++ ext_tables.php
@@ -1,6 +1,6 @@
 <?php
 if (!defined('TYPO3_MODE')) {
-	die('Access denied.');
+    die('Access denied.');
 }
 
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['Backend\Template\Components\ButtonBar']['getButtonsHook'][] = 'Goran\SaveCloseCe\Hooks\SaveCloseHook->addSaveCloseButton';
